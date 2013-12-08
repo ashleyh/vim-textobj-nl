3cd4803156ec9811dc83af08c2c0247b5b06e7a4
aph <a.hewson@gmail.com>
2013-12-08 18:28:11 +0000

    travis

--- test/run.sh
+++ test/run.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 set -e
+
+usage() {
+  echo "$0" "[--headless]"
+  exit 1
+}
+
 cd "$(dirname "$0")"
+
+interactive=yes
+while [[ $# -gt 0 ]] ; do
+  case "$1" in
+    --headless) unset interactive ;;
+    *) usage ;;
+  esac
+  shift
+done
+
 if [[ ! -d deps ]] ; then
   urls=(
     https://github.com/kana/vim-textobj-user
@@ -8,11 +24,20 @@ if [[ ! -d deps ]] ; then
   )
   echo "Test dependencies missing; I'll git clone the following urls for you:"
   for url in "${urls[@]}" ; do echo "   $url" ; done
-  echo "Press enter to continue or ^C to cancel:"
-  read
+  if [[ -n "$interactive" ]] ; then
+    echo "Press enter to continue or ^C to cancel:"
+    read
+  fi
   mkdir deps
   pushd deps
   for url in "${urls[@]}" ; do git clone "$url" ; done
   popd
 fi
-vim -u test.vimrc -c Vader test.vader
+
+if [[ -n "$interactive" ]] ; then
+  cmd="Vader"
+else
+  cmd="Vader!"
+fi
+
+vim -u test.vimrc -c "$cmd" test.vader
